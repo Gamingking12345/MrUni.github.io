6904f2289278ecc2050e20e522ef1a450d1e6028
Gamingking12345 <98438259+Gamingking12345@users.noreply.github.com>
2025-03-02 16:05:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 function showYourSecrets(){
-    alert("No New Content")
+    alert("No New Content");
 }
 
+window.addEventListener("hashchange", function() {
+    if (window.location.hash === "#aboutpg") {
+        document.body.classList.add("no-scroll");
+    } else {
+        document.body.classList.remove("no-scroll");
+    }
+});
